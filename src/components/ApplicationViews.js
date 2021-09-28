48b2b23e8df02593866e75c9217771de47cbe265
Michael Wright <info@saymoremultimedia.com>
2021-09-28 11:35:38 -0500

    Got the Locations, Customers and Employees links display their matching resources when their Nav links are clicked.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { LocationCard } from "./location/LocationCard"
+import { CustomerCard } from "./customer/CustomerCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
 
 export const ApplicationViews = () => {
     return (
@@ -14,6 +17,21 @@ export const ApplicationViews = () => {
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals">
               <AnimalCard />
+            </Route>
+
+            {/* Render the locations list when http://localhost:3000/locations */}
+            <Route path="/locations">
+              <LocationCard />
+            </Route>
+
+            {/* Render the customers list when http://localhost:3000/customers */}
+            <Route path="/customers">
+              <CustomerCard />
+            </Route>
+
+            {/* Render the employees list when http://localhost:3000/employees */}
+            <Route path="/employees">
+              <EmployeeCard />
             </Route>
         </>
     )
