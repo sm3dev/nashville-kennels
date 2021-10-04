5e88c45eccc4bf8ec69f320119741ec9b615bbd0
Michael Wright <info@saymoremultimedia.com>
2021-10-04 14:30:07 -0500

    completed Challenge Active NavBar Route

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -8,7 +8,7 @@ export const LocationCard = ({location, handleDeleteLocation}) => (
         <div class="card-content">
     <h3 className="location__name">{location.name}</h3>
     <div className="location__address">Address: {location.address}</div>
-    <button type="button" onClick={() => handleDeleteLocation(location.id)}>Delete Location</button>
+    <button type="button" onClick={() => handleDeleteLocation(location.id)}>Close This Location</button>
     <Link to={`/locations/${location.id}`}>
         <button>Details</button>
         </Link>
