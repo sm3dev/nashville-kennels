9be9e6310f4c162cc8e81d537b1ad5e1a3c4f36f
Michael Wright <info@saymoremultimedia.com>
2021-10-04 11:47:01 -0500

    AnimalForm works -- new animals can be input and sent to the database. I want to add the ability to upload image later

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,30 +1,29 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { Home } from "./Home"
-import { AnimalList } from "./animal/AnimalList"
-import { AnimalDetail } from "./animal/AnimalDetail"
-import { CustomerList } from "./customer/CustomerList"
-import { Locationlist } from "./location/LocationList"
-import { EmployeeList } from "./employee/EmployeeList"
-import { LocationDetail } from "./location/LocationDetail"
+import React from "react";
+import { Route } from "react-router-dom";
+import { Home } from "./Home";
+import { AnimalList } from "./animal/AnimalList";
+import { AnimalDetail } from "./animal/AnimalDetail";
+import { CustomerList } from "./customer/CustomerList";
+import { Locationlist } from "./location/LocationList";
+import { EmployeeList } from "./employee/EmployeeList";
+import { LocationDetail } from "./location/LocationDetail";
+import { AnimalForm } from './animal/AnimalForm'
 
 export const ApplicationViews = ({ isAdmin, myUser }) => {
   return (
     <>
       {/* Render the location list when http://localhost:3000/ */}
       <Route exact path="/">
-        <Home isAdmin={isAdmin} myUser={myUser}/>
+        <Home isAdmin={isAdmin} myUser={myUser} />
       </Route>
-
       {/* Render the animal list when http://localhost:3000/animals */}
       <Route exact path="/animals">
         <AnimalList />
       </Route>
-
       <Route path="/animals/:animalId(\d+)">
         <AnimalDetail />
       </Route>
-        {/*
+      {/*
           This is a new route to handle a URL with the following pattern:
           http://localhost:3000/animals/1
 
@@ -32,21 +31,23 @@ export const ApplicationViews = ({ isAdmin, myUser }) => {
           matches only numbers after the final slash in the URL
           http://localhost:3000/animals/jack
         */}
+      // Our shiny new route.
+      <Route path="/animals/create">
+        <AnimalForm />
+      </Route>
+
 
       {/* Render the locations list when http://localhost:3000/locations */}
       <Route exact path="/locations">
         <Locationlist />
       </Route>
-
       <Route path="/locations/:locationId(\d+)">
         <LocationDetail />
       </Route>
-
       {/* Render the customers list when http://localhost:3000/customers */}
       <Route path="/customers">
         <CustomerList />
       </Route>
-
       {/* Render the employees list when http://localhost:3000/employees */}
       <Route path="/employees">
         <EmployeeList />
--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -10,12 +10,25 @@ export const getAnimalById = (animalId) => {
 
 // get all Animals
 export const getAllAnimals = () => {
-  return fetch(`${remoteURL}/animals/?_expand=location&_expand=customer`).then((res) => res.json());
+  return fetch(`${remoteURL}/animals/?_expand=location&_expand=customer`).then(
+    (res) => res.json()
+  );
 };
 
 // delete an animal by Id
 export const deleteAnimal = (id) => {
   return fetch(`${remoteURL}/animals/${id}`, {
-    method: "DELETE"
-  }).then(result => result.json())
-}
+    method: "DELETE",
+  }).then((result) => result.json());
+};
+
+// add new animal
+export const addAnimal = (newAnimal) => {
+  return fetch(`${remoteURL}/animals`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newAnimal),
+  }).then((response) => response.json());
+};
